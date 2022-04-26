95c25fd126674e342dda6a08852bc2d9055c8f6b
lucia <ruadaslucia@gmail.com>
2022-04-26 08:51:02 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  //useStates
+
+  //funciones
+  return (
+    <div className="App">
+      <p>hola</p>
+      {/*todo html con interpolaciones/ listeners/ etc*/}
+    </div>
+  );
+}
+
+export default App;
