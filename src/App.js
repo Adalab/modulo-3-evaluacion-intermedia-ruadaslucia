5d16b033efd48bf93e8339235f0905d740e8e5af
lucia <ruadaslucia@gmail.com>
2022-04-26 09:14:00 +0200

    done structure filters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 
 function App() {
+  const url =
+    'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json';
   //useStates
 
   //funciones
   return (
     <div className="App">
-      <p>hola</p>
-      {/*todo html con interpolaciones/ listeners/ etc*/}
+      <h1>Friends quotes</h1>
+      <div className="containerFilters">
+        <div className="inputfilterName">
+          <p>Filter by quote: </p>
+          <input type="text" value="" />
+        </div>
+        <div className="selectFilterCharacter">
+          <p>Filter by character: </p>
+          <select name="select" id="">
+            <option value="all">All characters</option>
+            <option value="Rachel">Rachel</option>
+            <option value="Phoebe">Phoebe</option>
+            <option value="Monica">Monica</option>
+            <option value="Joey">Joey</option>
+            <option value="Ross">Ross</option>
+            <option value="Chandler">Chandler</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
